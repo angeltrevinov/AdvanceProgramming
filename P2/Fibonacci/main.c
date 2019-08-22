0d9c1553245ab2dd578ac9cbb3c324d823357370
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-22 09:03:17 -0500

    First Commit

---
+++ P2/Fibonacci/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//
+// Author: Angel Odiel Treviño Villanueva
+// Matricula: A01336559
+// Fecha_ 22/08/2019
+// A Program that prints the fibonacci sequence without using
+//  recursion, the user will tell how many numbers to pring
+//
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
