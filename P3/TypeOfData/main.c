741938ccfbdc7fff6dcc859674266467c738a527
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-23 12:29:34 -0500

    Finishing 3 practice

--- P3/TypeOfData/main.c
+++ P3/TypeOfData/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <float.h>
 
 /*
  * Author: Angel Odiel Treviño Villanueva
@@ -35,17 +36,12 @@ int main() {
            charCharacter
            );
     printf("your double %lf storage size is %d bytes, "
-           "I can read any number from",
+           "I can read any number from %lf to %lf",
             dDouble,
-           sizeof(dDouble)
+           sizeof(dDouble),
+           DBL_MIN,
+           DBL_MAX
            );
 
     return 0;
 }
-
-/*
- *An integer
- * Your integer < > storage size is <<size>> bytes.
- * Your char < > storage size is <<size>> bytes. And I can read it as <<display it as character>> or as <<display it as integer>>.
- * Your double < > storage size is <<size>> bytes, I can read any number from <<Min positive value>> to <<Max positive value>> in this data type.
- * */
