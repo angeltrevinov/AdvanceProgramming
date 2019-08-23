784c3132d6bf40dd818a45d6a40223720b7b9aec
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-23 11:21:01 -0500

    Printing to char

--- P3/TypeOfData/main.c
+++ P3/TypeOfData/main.c
@@ -8,7 +8,30 @@
  *      types of data.
  * */
 
+//----------------------------------------------------------
 int main() {
-    printf("Hello, World!\n");
+    int intInteger;
+    char charCharacter;
+    double dDouble;
+
+    printf("Enter an integer\n");
+    scanf("%d", &intInteger);
+
+    printf("Enter a character\n");
+    scanf(" %c", &charCharacter);
+
+    printf("Enter your double\n");
+    scanf("%lf", &dDouble);
+
+    printf("Your integer %d storage size is %d bytes\n", intInteger, sizeof(intInteger));
+    printf("Your char %c your storage size is %d bytes. And I can read it as %c or as a %d \n", charCharacter, sizeof(charCharacter), charCharacter, charCharacter);
+
     return 0;
 }
+
+/*
+ *An integer
+ * Your integer < > storage size is <<size>> bytes.
+ * Your char < > storage size is <<size>> bytes. And I can read it as <<display it as character>> or as <<display it as integer>>.
+ * Your double < > storage size is <<size>> bytes, I can read any number from <<Min positive value>> to <<Max positive value>> in this data type.
+ * */
