370f953e70c5b1f4de5e53ad2c8a2deb57443ec9
Angel Trevino <angel.trevinov@gmail.com>
2019-08-22 21:13:19 -0500

    Adding practice 3

---
+++ P3/TypeOfData/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Author: Angel Odiel Treviño Villanueva
+ * Matricula: A01336559
+ * Fecha: 22/08/2019
+ * A program that displays the different sizes of
+ *      types of data.
+ * */
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
