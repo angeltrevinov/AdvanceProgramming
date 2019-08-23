44fd82709d80d885e308bb275905662c57d62a97
Angel Trevino <angel.trevinov@gmail.com>
2019-08-22 20:04:27 -0500

    Finishing the Fibonacci sequence

--- P2/Fibonacci/main.c
+++ P2/Fibonacci/main.c
@@ -8,7 +8,47 @@
 //  recursion, the user will tell how many numbers to pring
 //
 
+//-------------------------------------------------------------------------
+void Fibonacci(
+        //Method that displays from the first to the Nth number of the
+        //      Fibonacci sequence
+
+        //How many numbers the user wants to print
+        int intHowMany
+        ) {
+
+   if(intHowMany == 0) {
+       return;
+   } else if(intHowMany == 1) {
+       printf("0 ");
+   } else {
+       // if the user wants to print more than one number
+
+       printf("0 ");
+       int intSum = 0;
+       int intPreSum = 0;
+       int intPreOldSum = 1;
+
+       for(
+               //prints the fibonacci from the first number
+               int intContador = 1; intContador < intHowMany; intContador++
+               ) {
+           intSum = intPreOldSum + intPreSum;
+           intPreOldSum = intPreSum;
+           intPreSum = intSum;
+           printf("%d ", intSum);
+       }
+   }
+}
+
+//-------------------------------------------------------------------------
 int main() {
-    printf("Hello, World!\n");
+
+    int intHowMany;
+    printf("How many numbers do you want from the sequence? \n");
+    scanf("%d", &intHowMany);
+
+    Fibonacci(intHowMany);
+
     return 0;
 }
