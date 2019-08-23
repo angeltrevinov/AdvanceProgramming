866831fc100e123edd35e53a5eb84a6dd2f8860b
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-08-23 12:51:00 -0500

    Starting P4

---
+++ P4/MatrixOperations/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Author: Angel Odiel Treviño Villanueva
+ * Matricula: A01336559
+ * Fecha: 22/08/2019
+ *
+ * A program that reads two 2x2 matrix and does Addition,
+ *      Substraction and Multiplication with them
+ * */
+
+//----------------------------------------------------------
+void FillinMatrix(int intMatrix[2][2]) {
+    for(int intCountY = 0; intCountY < 2; intCountY++){
+
+    }
+}
+
+//----------------------------------------------------------
+int main() {
+
+    int intFirstMatrix[2][2];
+    int intSecondMatrix[2][2];
+
+    printf("Hello, World!\n");
+    return 0;
+}
