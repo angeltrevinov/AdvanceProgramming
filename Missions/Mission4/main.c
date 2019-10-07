047176c2bea8af0a6872323a1f1770444c9562c4
Angel Treviño <angel.trevinov@gmail.com>
2019-10-07 02:55:43 -0500

    Finishing Mission4

--- Missions/Mission4/main.c
+++ Missions/Mission4/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <windows.h>
+#include <winuser.h>
+#include <conio.h>
 
 /*
  * Author: Angel Odiel Treviño Villanueva
@@ -10,6 +13,32 @@
 
 //----------------------------------------------------------
 int main() {
-    printf("Hello, World!\n");
+
+    int intX, intY, intRun = 0;
+
+    //the pointer of the file
+    FILE *fptr = fopen("Coordinates.txt", "ab+");
+    if(fptr == NULL) {
+        printf("There was a problem creating the file");
+        return -1;
+    }
+
+    while(intRun == 0 ) {
+        Sleep(100);
+        if((GetKeyState(VK_LBUTTON) & 0x80) != 0) {
+
+            POINT Cursor;
+            GetCursorPos(&Cursor);
+            intX = Cursor.x;
+            intY = Cursor.y;
+
+            fprintf(fptr, "positionX: %d, positionY: %d \n", intX, intY);
+            printf("positionX: %d, positionY: %d \n", intX, intY);
+        } else if((GetKeyState(VK_RBUTTON) & 0x80) != 0) {
+            intRun =1;
+        }
+    }
+
+    fclose(fptr);
     return 0;
 }
